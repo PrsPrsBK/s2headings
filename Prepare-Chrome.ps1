a63a281da563b2eafcc10da0b998f4f4aa8495e0
PrsPrsBK <prsprsbk@gmail.com>
2021-03-25 06:24:25 +0900

    cosmetics: remove Prepare-Chrome.ps1

--- Prepare-Chrome.ps1
+++
@@ -1,6 +0,0 @@
-$script_path =  (Split-Path -Parent $MyInvocation.MyCommand.Path)
-
-New-Item ./lib -ItemType "directory" -Force | Out-Null
-
-Copy-Item ./node_modules/webextension-polyfill/dist/browser-polyfill.min.js ./lib/
-
